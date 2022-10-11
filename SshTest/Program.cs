e763b556a718171a505ceee92218f8a0016d3888
uvc-vivian <viviankim@uvc.co.kr>
2022-10-11 17:54:09 +0900

    first commit

---
+++ SshTest/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Text.RegularExpressions;
+using Renci.SshNet;
+using Renci.SshNet.Sftp;
+using Renci.SshNet.Common;
+
+namespace SshNet
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string address = "10.20.193.62";
+            int port = 4052;
+            string user = "advantech";
+            string password = "opcua5497";
+            AccessSsh(address, port, user, password);
+
+        }
+
+        static void AccessSsh(string address, int port, string user, string password)
+        {
+            try
+            {
+                //create new client & session
+                SshClient client = new SshClient(address, port, user, password);
+                client.Connect();
+
+                //create terminal -> used by ShellStream
+                //create a dictionary of terminal modes & add terminal mode
+                IDictionary<TerminalModes, uint> termkvp = new Dictionary<TerminalModes, uint>();
+                termkvp.Add(TerminalModes.ECHO, 53);
+
+                //execute start.sh script
+                ShellStream shellStream = client.CreateShellStream("xterm", 80, 24, 800, 600, 1024, termkvp);
+                var output = shellStream.Expect(new Regex(@"[$>]"));
+                //shellStream.WriteLine("cd /home/edge/Huyundai/Doosan");
+                //shellStream.WriteLine("ls");
+                shellStream.WriteLine("sudo sh /home/edge/Huyundai/Doosan/start.sh");
+                output = shellStream.Expect(new Regex(@"([$#>:])"));
+                shellStream.WriteLine(password);
+
+                string line;
+                while((line = shellStream.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
