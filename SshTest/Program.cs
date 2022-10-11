bbab41257bdeeb5894dbc4f3a509b6e30ee12f11
joy <joy@uvc.co.kr>
2022-10-11 22:24:45 +0900

    add kuka

--- SshTest/Program.cs
+++ SshTest/Program.cs
@@ -17,6 +17,12 @@ namespace SshNet
             string password = "opcua5497";
             AccessSsh(address, port, user, password);
 
+            string KUKAaddress = "10.20.193.101";
+            int KUKAport = 22;
+            string KUKAuser = "hyundai";
+            string KUKApw = "opcua5497";
+            AccessKUKASsh(KUKAaddress, KUKAport, KUKAuser, KUKApw);
+
         }
 
         static void AccessSsh(string address, int port, string user, string password)
@@ -43,6 +49,40 @@ namespace SshNet
 
                 string line;
                 while((line = shellStream.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Console.WriteLine(ex.ToString());
+            }
+        }
+
+        static void AccessKUKASsh(string address, int port, string user, string password)
+        {
+            try
+            {
+                //create new client & session
+                SshClient client = new SshClient(address, port, user, password);
+                client.Connect();
+
+                //create terminal -> used by ShellStream
+                //create a dictionary of terminal modes & add terminal mode
+                IDictionary<TerminalModes, uint> termkvp = new Dictionary<TerminalModes, uint>();
+                termkvp.Add(TerminalModes.ECHO, 53);
+
+                //execute start.sh script
+                ShellStream shellStream = client.CreateShellStream("xterm", 80, 24, 800, 600, 1024, termkvp);
+                var output = shellStream.Expect(new Regex(@"[$>]"));
+                //shellStream.WriteLine("cd /home/edge/Huyundai/Doosan");
+                //shellStream.WriteLine("ls");
+                shellStream.WriteLine("sudo sh /home/hyundai/Hyundai-KUKA/run.sh");
+                output = shellStream.Expect(new Regex(@"([$#>:])"));
+                shellStream.WriteLine(password);
+
+                string line;
+                while ((line = shellStream.ReadLine()) != null)
                 {
                     Console.WriteLine(line);
                 }
